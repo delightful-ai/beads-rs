cff03fc0b5375cd14104e7e05e2be233a65250cc
Darin Kishore <darinkishore@protonmail.com>
2025-12-13 21:26:03 -0800

    chore: bump version to 0.1.9

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
         # The beads-rs package
         beads-rs = pkgs.rustPlatform.buildRustPackage {
           pname = "beads-rs";
-          version = "0.1.8";
+          version = "0.1.9";
 
           src = ./.;
 
