0e1698da1c1184cf9538ca5529a68214caccab7d
Darin Kishore <darinkishore@protonmail.com>
2026-01-13 18:39:12 -0800

    bd-3m5.48: export replication server module

--- src/daemon/repl/mod.rs
+++ src/daemon/repl/mod.rs
@@ -5,6 +5,7 @@ pub mod gap_buffer;
 pub mod manager;
 pub mod peer_acks;
 pub mod proto;
+pub mod server;
 pub mod session;
 pub mod store;
 
@@ -18,6 +19,9 @@ pub use peer_acks::{PeerAckError, PeerAckTable, QuorumOutcome};
 pub use proto::{
     Ack, Capabilities, Events, Hello, ProtoDecodeError, ProtoEncodeError, ReplEnvelope,
     ReplMessage, Want, WatermarkHeads, WatermarkMap, decode_envelope, encode_envelope,
+};
+pub use server::{
+    ReplicationServer, ReplicationServerConfig, ReplicationServerError, ReplicationServerHandle,
 };
 pub use session::{
     IngestOutcome, ProtocolRange, Session, SessionAction, SessionConfig, SessionPeer, SessionPhase,
