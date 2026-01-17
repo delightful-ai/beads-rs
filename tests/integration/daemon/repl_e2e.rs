306d7a1b0c8783e97ad7e1400cf1cfeb10d5f37f
Darin Kishore <darinkishore@protonmail.com>
2026-01-16 19:41:18 -0800

    bd-drtn: widen repl e2e timeouts

--- tests/integration/daemon/repl_e2e.rs
+++ tests/integration/daemon/repl_e2e.rs
@@ -14,17 +14,14 @@ fn repl_daemon_to_daemon_roundtrip() {
 
     let rig = ReplRig::new(3, options);
 
-    let ids = [
-        rig.create_issue(0, "from-0"),
-        rig.create_issue(1, "from-1"),
-        rig.create_issue(2, "from-2"),
-    ];
+    let id0 = rig.create_issue(0, "from-0");
+    rig.wait_for_show(1, &id0, Duration::from_secs(15));
 
-    for issue_id in &ids {
-        rig.wait_for_show(0, issue_id, Duration::from_secs(6));
-        rig.wait_for_show(1, issue_id, Duration::from_secs(6));
-        rig.wait_for_show(2, issue_id, Duration::from_secs(6));
-    }
+    let id1 = rig.create_issue(1, "from-1");
+    rig.wait_for_show(2, &id1, Duration::from_secs(15));
 
-    rig.assert_converged(&[NamespaceId::core()], Duration::from_secs(10));
+    let id2 = rig.create_issue(2, "from-2");
+    rig.wait_for_show(0, &id2, Duration::from_secs(15));
+
+    rig.assert_converged(&[NamespaceId::core()], Duration::from_secs(30));
 }
