1b4c646a48974d8274d2528546744d68c58a7ba5
Darin Kishore <darinkishore@protonmail.com>
2026-01-16 20:00:39 -0800

    bd-drtn: assert repl convergence under tailnet

--- tests/integration/daemon/repl_e2e.rs
+++ tests/integration/daemon/repl_e2e.rs
@@ -3,23 +3,27 @@
 use std::time::Duration;
 
 use crate::fixtures::repl_rig::{FaultProfile, ReplRig, ReplRigOptions};
+use beads_rs::core::NamespaceId;
 
 #[test]
 fn repl_daemon_to_daemon_roundtrip() {
     let mut options = ReplRigOptions::default();
-    options.fault_profile = Some(FaultProfile::none());
+    options.fault_profile = Some(FaultProfile::tailnet());
     options.seed = 7;
 
     let rig = ReplRig::new(3, options);
 
-    let id0 = rig.create_issue(0, "from-0");
-    rig.wait_for_show(1, &id0, Duration::from_secs(15));
+    let ids = [
+        rig.create_issue(0, "from-0"),
+        rig.create_issue(1, "from-1"),
+        rig.create_issue(2, "from-2"),
+    ];
 
-    let id1 = rig.create_issue(1, "from-1");
-    rig.wait_for_show(2, &id1, Duration::from_secs(15));
+    for node_idx in 0..3 {
+        for id in &ids {
+            rig.wait_for_show(node_idx, id, Duration::from_secs(30));
+        }
+    }
 
-    let id2 = rig.create_issue(2, "from-2");
-    rig.wait_for_show(0, &id2, Duration::from_secs(15));
-
-    rig.assert_peers_seen(Duration::from_secs(30));
+    rig.assert_converged(&[NamespaceId::core()], Duration::from_secs(60));
 }
