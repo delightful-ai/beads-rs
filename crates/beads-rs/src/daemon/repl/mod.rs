49ec3627fb31c93e26ae29c90c7b98454353f2c6
Darin Kishore <darinkishore@protonmail.com>
2026-01-27 05:34:35 -0800

    bd-zp59: encode watermark snapshot invariants in proto

--- crates/beads-rs/src/daemon/repl/mod.rs
+++ crates/beads-rs/src/daemon/repl/mod.rs
@@ -24,7 +24,7 @@ pub use manager::{
 pub use peer_acks::{PeerAckError, PeerAckTable, QuorumOutcome};
 pub use proto::{
     Ack, Capabilities, Events, Hello, ProtoDecodeError, ProtoEncodeError, ReplEnvelope,
-    ReplMessage, Want, WatermarkHeads, WatermarkMap, decode_envelope, encode_envelope,
+    ReplMessage, Want, WatermarkMap, WatermarkState, decode_envelope, encode_envelope,
 };
 pub use runtime::{ReplIngestRequest, ReplSessionStore, WalRangeError, WalRangeReader};
 pub use server::{
